bf039e44a240410aea64f951ef70fbcd0d8d74b3
Arafat Husayn <hello@arafat.dev>
2023-05-15 21:22:16 +0600

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { PropsWithChildren } from "react";
 
 export const metadata = {
-  title: "Supercharged Next.js App",
-  description: "Generated from supercharged Next.js template.",
+  title: "Next.js App",
+  description: "Generated from simple Next.js template.",
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
